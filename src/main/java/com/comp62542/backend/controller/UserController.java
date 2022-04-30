e7b77d185a32fde95d566696cce3b7a46a9f737c
仇剑昆 <jiankun.qiu@postgrad.manchester.ac.uk>
2022-04-30 18:44:29 +0100

    update login controller

--- src/main/java/com/comp62542/backend/controller/UserController.java
+++ src/main/java/com/comp62542/backend/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.comp62542.backend.controller;
 
 import com.comp62542.backend.service.UserService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,11 @@ public class UserController {
     public Map<String, Object> login(String studentId, HttpServletResponse response) {
         Map<String, Object> map = new HashMap<>();
         Map<String, Object> loginMap = userService.login(studentId);
-        map.put("status", loginMap.get("status"));
+        if(StringUtils.isBlank(studentId)) {
+            map.put("status", 0);
+        }else {
+            map.put("status", loginMap.get("status"));
+        }
         map.put("user", loginMap.get("user"));
         if(loginMap.containsKey("token")) {
             response.addHeader("token", (String) loginMap.get("token"));
